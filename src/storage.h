51f6f4252f5e43809a87b9c92224cd89b5216445
stolevegen <59141609+stolevegen@users.noreply.github.com>
2025-09-19 10:04:10 +0200

    Update storage.h
    Added readString and writeString to accomodate setting SSID/password through WebUI

--- src/storage.h
+++ src/storage.h
@@ -5,5 +5,8 @@ void setup_storage(void);
 
 esp_err_t readFloat(char* key, float *value, float defaultValue);
 esp_err_t writeFloat(char* key, float value);
+esp_err_t readString(const char* key, char* out, size_t out_len, const char* def);
+esp_err_t writeString(const char* key, const char* value);
+
 
 #endif
