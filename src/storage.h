aecda567c98293e9e5a7ae643a266a86eb2718f0
stolevegen <59141609+stolevegen@users.noreply.github.com>
2025-09-19 12:49:16 +0200

    Update storage.h
    add runtime logger

--- src/storage.h
+++ src/storage.h
@@ -7,6 +7,8 @@ esp_err_t readFloat(char* key, float *value, float defaultValue);
 esp_err_t writeFloat(char* key, float value);
 esp_err_t readString(const char* key, char* out, size_t out_len, const char* def);
 esp_err_t writeString(const char* key, const char* value);
+esp_err_t readUInt64(const char* key, uint64_t* out, uint64_t def);
+esp_err_t writeUInt64(const char* key, uint64_t value);
 
 
 #endif
